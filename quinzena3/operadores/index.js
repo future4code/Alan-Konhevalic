4bd9ad6af8d47973c8ae835fdc541b3fb26fb1e7
Alan Konhevalic <alan.patrick.konhevalic@gmail.com>
2020-09-17 19:35:41 -0300

    exercicios javascript de operadores

---
+++ quinzena3/operadores/index.js
@@ -0,0 +1,97 @@
+console.log("Oie, eu só sirvo pra saber se as coisas funcionaram!")
+
+
+/*
+
+1.a) false
+1.b) false
+1.c) true
+1.d) boolean
+
+*/
+
+
+/*
+
+2.a) undefined
+2.b) null
+2.c) 11
+2.d) 3
+2.e) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+2.f) 9
+
+
+*/
+
+/* Escrita de código: */
+
+/*
+//1.a)
+let idade = Number(prompt("Qual sua idade?"))
+
+//1.b)
+let idadeAmigo = Number(prompt("Qual a idade do seu melhor amigo?"))
+
+//1.c)
+idade >= idadeAmigo == true
+let result = idade > idadeAmigo
+console.log("Sua idade é maior que a do seu amigo?", result)
+
+//1.d)
+let diferenca = idade - idadeAmigo
+console.log("A diferença de idade é: ", diferenca)
+*/
+
+
+/*
+//2.a)
+
+let numero = Number(prompt("Insira um número par: "))
+
+//2.b)
+let resto = numero%2
+console.log(resto)
+
+//2.c)
+//O resto sempre será 0 
+
+
+//2.d)
+//O resto sempre será 1.
+*/
+
+/*
+//3.a)
+let listaDeTarefas = []
+
+//3.b)
+let pergunta1 = prompt("Você tomou banho hoje?")
+let pergunta2 = prompt("Você entregou a atividade hoje?")
+let pergunta3 = prompt("Você descansou hoje?")
+
+listaDeTarefas[0] = pergunta1
+listaDeTarefas[1] = pergunta2
+listaDeTarefas[2] = pergunta3
+
+//3.c)
+console.log(listaDeTarefas)
+
+//3.d)
+let tarefaRealizada = Number(prompt("Qual tarefa você já realizou hoje? Digite 0, 1 ou 2"))
+
+//3.e)
+listaDeTarefas.splice(tarefaRealizada, 1)
+
+
+console.log(listaDeTarefas)
+*/
+
+/*
+//4)
+let nome = prompt("Qual seu nome?")
+let email = prompt("Qual seu email?")
+
+alert("O email " + ' ' + email + ' ' + "foi cadastrado com sucesso. Seja bem-vindo(a)," + ' ' + nome)
+*/
+
+
