967b3bf978f0abeaeafadcc67531e10786c4f788
Alan Konhevalic <alan.patrick.konhevalic@gmail.com>
2020-10-13 11:28:09 -0300

    exercicios de interpretacao de codigos

---
+++ quinzena4/lista-de-exercicios/index.js
@@ -0,0 +1,15 @@
+/**
+ * Exercício de interpretação de código:
+ * 
+ * 1)A função recebe um valor, emite um alert para o usuario digitar a cotação, e retorna o valor em reais.
+ * 
+ * 2)A função recebe dois parametros, um para o tipo de investimento, e o valor a ser investido. Com isso, é feito condicionais
+ *   para multiplicar o valor a ser investido conforme o tipo de investimento. para novoMontante, será R$150 multiplicado por 1.1, por exemplo
+ * 
+ * 3) O código adiciona no array1 os números pares e no array2 os numeros impares do array "numeros"
+ * 
+ * 4)O código irá analisar os valores do array "numeros" e irá percorrer cada elemento. Irá imprimir na variavel numero1 o o menor valor do array
+ *   e na variavel numero2 irá imprimir o maior valor do array
+ */
+
+
